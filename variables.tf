8a47b9ce93b34d52aa16e0ed7d6ab434dba90a53
youngwjung <youngwjung@gmail.com>
2026-05-11 07:27:40 +0000

    변수에 기본값 지정

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "vpc_cidr" {
   description = "VPC 대역대"
   type        = string
+  default     = "10.100.0.0/24"
 }
 
 variable "num_node" {
   description = "노드 갯수"
   type        = number
+  default     = 3
 }
